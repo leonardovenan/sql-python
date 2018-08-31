fdacf6a606824a99ce16875ebe0c59bf5fd42183
LEONARDO VENANCIO CORREIA <leonardovenancio12@gmail.com>
2018-08-31 09:54:18 -0300

    Add files via upload

--- ler_banco.py
+++ ler_banco.py
@@ -9,14 +9,14 @@ import matplotlib.pyplot as plt
 
 lista_data=[]
 sumValLiq = []
-
+print ("connecting to the database...")
 con = pymysql.connect(host='localhost',
                              user='root',
                              password='1234567',
                              db='sgi_producao',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
+print ("Connected")
 cursor = con.cursor()
 
 
